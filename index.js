357be80098c99e1907b436761b127717700439b0
thereeesah <theresa_n123@hotmail.com>
2019-07-16 22:23:24 -0400

    Test PDF Report

--- index.js
+++ index.js
@@ -1,51 +1,64 @@
-const mongoose = require('mongoose');
-var i = 0;
+// const mongoose = require('mongoose');
+// var i = 0;
 
-mongoose.connect('mongodb://localhost/test');
+// var Venture_Name_List;
+// var Contact_List;
+// var Website_List;
+// var OneLiner_List;
+// var Description_List;
 
-const testSchema = new mongoose.Schema( {
-        testField: { type: String, required: true }
-    }
-)
+// mongoose.connect('mongodb://localhost/test');
 
-const testModel = mongoose.model('Test', testSchema);
+// const ReportTest = new mongoose.Schema( {
+//         Venture_Name: { type: String, required: true },
+//         Contact: {type: String, required: true},
+//         Website: {type: String, required: true},
+//         OneLiner: {type: String, required: true},
+//         Description: {type: String, required: true}
+//     }
+// )
 
-const addTestObject = async () => {
-    try {
-        const newTest = new testModel({ testField: "Hello World!"});
-        await newTest.save().exec();
-    } catch (err) {
-        console.log(err.message);
-    }
-}
+// const testModel = mongoose.model('Test', ReportTest);
 
-//a new change
+// const addTestObject = async () => {
+//     try {
+//         const newTest = new testModel({ Venture_Name: "Tesla", Contact: "tesla@gmail.com", Website: "www.tesla.com", OneLiner: "For a sustainable future", Description: "A company for xxx"});
+//         await newTest.save().exec();
+//     } catch (err) {
+//         console.log(err.message);
+//     }
+// }
 
-async function getTestObjects() {
-    try {
-        const testObjects = await testModel.find().exec();
-        //console.log(testObjects[0].testField);
+// async function getTestObjects() {
+//     try {
+//         const testObjects = await testModel.find().exec();
+//         //console.log(testObjects[0].Venture_Name);
 
-        for (i = 0; i < testObjects.length; i++) {
-            console.log("object number: " + i + " | object itself: " + testObjects[0].testField);
+//         for (i = 0; i < testObjects.length; i++) {
+//             console.log("object number: " + i + " | object itself: " + testObjects[i].Venture_Name + " " + testObjects[i].Contact + " " + testObjects[i].Website + " " + testObjects[i].OneLiner + " " + testObjects[i].Description + " ");
+//             Venture_Name_List.push(testObjects[i].Venture_Name);
+//             Contact_List.push(testObjects[i].Contact);
+//             Website_List.push(testObjects[i].Website);
+//             OneLiner_List.push(testObjects[i].OneLiner);
+//             Description_List.push(testObjects[i].Descriptiion);
+//         }
 
-        }
-
-        console.log("im good");
-        mongoose.disconnect('mongodb://localhost/test');
+//         console.log("Attaining Objects Complete");
+//         mongoose.disconnect('mongodb://localhost/test');
         
-    } catch (error) {
+//     } catch (error) {
         
-        console.log(error.message);
-    }
-}
+//         console.log(error.message);
+//     }
+// }
 
-addTestObject();
-getTestObjects();
+// //TODO: await function does not know how to turn off
 
-console.log("im done");
+// addTestObject();
+// getTestObjects();
 
-var http = require('http');
+// Attempting to write in an HTML document
+/*var http = require('http');
 
 var fileSystem = require('fs');
 
@@ -65,4 +78,23 @@ var server = http.createServer(function(req, resp){
 
 server.listen(8080);
 
-console.log("listewning 8080");
+console.log("listewning 8080"); */
+
+/****************PDF TESTING*************/
+
+//wait 
+
+const PDFDocument = require('pdfkit');
+const fs = require('fs');
+
+const doc = new PDFDocument;
+doc.pipe(fs.createWriteStream('ReportTest.pdf'));
+
+doc.addPage()
+    .text('Venture Name:')// + Venture_Name_List)
+    .text('Contact:')// + Contact_List)
+    .text('Website:')// + Website_List)
+    .text('One Liner')// + OneLiner_List)
+    .text('Description');// + Description_List);
+
+doc.end();
